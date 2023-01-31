44e2ef2dc1303a8059d2fce036bf744bfda56d78
PiniCohen1 <123373386+PiniCohen1@users.noreply.github.com>
2023-01-31 20:20:33 +0200

    Add doc comment

--- AspectsExample/src/main/java/com/cognyte/aspectsexample/aop/ExceptionHandlerAspect.java
+++ AspectsExample/src/main/java/com/cognyte/aspectsexample/aop/ExceptionHandlerAspect.java
@@ -12,15 +12,19 @@ import java.util.*;
 @Aspect
 public class ExceptionHandlerAspect {
 
-    // Use Value annotation for initialize mail list from property file
-    // specific property file defined in other annotation (@PropertySource) which
-    // defined in MainConf
+    /**
+     * Use Value annotation for initialize mail list from property file
+     * specific property file defined in other annotation (@PropertySource) which
+     * defined in MainConf
+     **/
     @Value("${support.mails}")
     private String[] mails;
 
-    // Using WeakHashMap to avoid keeping unreferenced Object in memory
-    // Otherwise, we could explode our memory with irrelevant exceptions
-    // which already handled
+    /**
+     * Using WeakHashMap to avoid keeping unreferenced Object in memory
+     * Otherwise, we could explode our memory with irrelevant exceptions
+     * which already handled
+     **/
     private final Map<CognyteException,Void> exceptions = new WeakHashMap<>();
 
     // Option 1 Using Around
