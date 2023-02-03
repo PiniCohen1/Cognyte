8a0bec93d702484b86a61ea25225e65bcc95f3f5
PiniCohen1 <123373386+PiniCohen1@users.noreply.github.com>
2023-02-03 14:46:58 +0200

    Aspect Sample parent pom

---
+++ AspectsExample/src/test/java/com/cognyte/aspectsexample/AspectsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognyte.aspectsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
